4804bfd2125bea8f25cae38cbf3849ddf10537dd
Alan Evans <thealanevans@gmail.com>
2023-08-12 19:43:34 -0400

    Unit tests

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os.path
 import re
+import subprocess
+from pathlib import Path
 
 from gvgen import GvGen
 from rich import print as rprint, inspect
@@ -30,18 +32,27 @@ def dependencies_to_graph(input_file: str, output_file: str):
 
 
 def load_graph(input_file: str):
-    graph = nx.DiGraph()
     with open(input_file, "r") as file:
-        for line in file.readlines():
-            search = re.search(r"(\S*) -> (\S*)", line)
-            if not search:
-                rprint("[red]Malformed input [line]")
-            node_a = search.group(1)
-            node_b = search.group(2)
-            rprint(f"[cyan]{node_a}[/cyan] depends on [cyan]{node_b}[/cyan]")
-            graph.add_edge(node_a, node_b)
-    rprint(graph)
+        lines = file.readlines()
+        return load_graph_from_lines(lines)
+
+
+def load_graph_from_lines(lines):
+    graph = nx.DiGraph()
+    for line in lines:
+        search = re.search(r"(\S*) -> (\S*)", line)
+        if not search:
+            fail("[red]Malformed input [line]")
+        u = search.group(1)
+        v = search.group(2)
+        # rprint(f"[cyan]{u}[/cyan] depends on [cyan]{v}[/cyan]")
+        graph.add_edge(u, v)
     return graph
+
+
+def fail(message):
+    rprint(message)
+    exit(1)
 
 
 def gen(graph: nx.DiGraph):
@@ -55,7 +66,10 @@ def gen(graph: nx.DiGraph):
         dot.dot(file)
 
 
-def gen_delta(graph_delta: nx.Graph, new_color="#158510", old_color="#ff0000"):
+def gen_delta(graph_delta: nx.Graph, new_color="#158510", old_color="#ff0000",
+              file=None):
+    if not file:
+        file = Path("output/graph2.dot")
     dot = GvGen()
     dot.styleDefaultAppend("shape", "rectangle")
     graph = graph_delta
@@ -84,7 +98,8 @@ def gen_delta(graph_delta: nx.Graph, new_color="#158510", old_color="#ff0000"):
             dot.propertyAppend(link, "color", color)
         if data.get("indirect"):
             dot.propertyAppend(link, "style", "dashed")
-    with open("output/graph.dot", "w") as file:
+    os.makedirs(Path(file).parent, exist_ok=True)
+    with open(file, "w") as file:
         dot.dot(file)
 
 
@@ -94,11 +109,13 @@ def compare_graph(older, newer):
     new_edges = newer.edges - older.edges
     removed_edges = older.edges - newer.edges
     graph = nx.DiGraph()
+    new_visible_graph = nx.DiGraph()
     visible_nodes = set()
     for u, v in new_edges:
         graph.add_edge(u, v)
         graph.edges[u, v]["new"] = True
         visible_nodes.update({u, v})
+        new_visible_graph.add_edge(u, v)
     for u, v in removed_edges:
         graph.add_edge(u, v)
         graph.edges[u, v]["old"] = True
@@ -109,12 +126,21 @@ def compare_graph(older, newer):
     old_nodes = older.nodes - newer.nodes
     for old_node in old_nodes:
         graph.nodes[old_node]["old"] = True
-    # Add edges for all affected nodes with indirect connections
+    # Add existing edges for visible nodes that are linked
     pairs = dict(nx.all_pairs_bellman_ford_path_length(newer))
-    rprint(pairs)
     for u in visible_nodes:
         for v in visible_nodes:
-            if ((pairs.get(u) or {}).get(v) or 0) > 1:
+            distance = ((pairs.get(u) or {}).get(v) or 0)
+            if distance == 1:
+                graph.add_edge(u, v)
+                new_visible_graph.add_edge(u, v)
+    # Add edges for all affected nodes with indirect connections
+    graph_paths = dict(nx.all_pairs_bellman_ford_path_length(new_visible_graph))
+    for u in visible_nodes:
+        for v in visible_nodes:
+            distance = ((pairs.get(u) or {}).get(v) or 0)
+            visible_distance = ((graph_paths.get(u) or {}).get(v) or 0)
+            if visible_distance == 0 and distance > 1:
                 graph.add_edge(u, v)
                 graph.edges[u, v]["indirect"] = True
 
@@ -122,7 +148,7 @@ def compare_graph(older, newer):
 
 
 def main():
-    dependencies_to_graph(input_file="output/dependencies.txt", output_file="output/sample.deps")
+    dependencies_to_graph(input_file="sample/dependencies.txt", output_file="output/sample.deps")
     g = load_graph(input_file="examples/revision1.deps")
     gen_delta(g)
 
@@ -135,8 +161,31 @@ def main():
     # gen_delta(g3)
 
 
+def run_tests(dir):
+    rprint(f"[yellow][bold]Running test suite: [cyan]{dir}[/cyan]")
+    for file_path in map(lambda p: os.path.join(dir, p), os.listdir(dir)):
+        if os.path.isfile(file_path):
+            rprint(f"[yellow][bold]Running test [cyan]{file_path}[/cyan]")
+            with open(file_path) as file:
+                lines = file.readlines()
+                if lines[0] != "> Before\n":
+                    fail('First line must be "> Before"')
+                after = lines.index("> After\n")
+                before = load_graph_from_lines(lines[1:after])
+                after = load_graph_from_lines(lines[after + 1:])
+                compared = compare_graph(before, after)
+                test_output_dot = Path(os.path.join("test_output", file_path)).with_suffix(".dot")
+                test_output_png = Path(os.path.join("output", file_path)).with_suffix(".png")
+                gen_delta(compared, file=test_output_dot)
+                os.makedirs(test_output_png.parent, exist_ok=True)
+                subprocess.run(["dot", "-Tpng", test_output_dot, "-o", test_output_png])
+        if os.path.isdir(file_path):
+            run_tests(file_path)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main()
+    # main()
+    run_tests("tests")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
