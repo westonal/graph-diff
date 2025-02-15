ff1f3f3bbb361a22f9c247aba9baea38c2aa141c
Alan Evans <thealanevans@gmail.com>
2025-02-15 12:22:05 -0500

    Rename test inputs

--- diff_dot/diff_render.py
+++ diff_dot/diff_render.py
@@ -1,4 +1,6 @@
 import os
+from dataclasses import dataclass
+from functools import cached_property
 from pathlib import Path
 
 import networkx as nx
@@ -6,42 +8,54 @@ import networkx as nx
 from .dot_file import Dot
 
 
+@dataclass
+class DotStyle:
+    new_color: str = "#158510"
+    old_color: str = "#ff0000"
+
+
 class Renderer(object):
 
-    def __init__(self, graph_delta: nx.Graph, new_color="#158510", old_color="#ff0000"):
+    def __init__(self, graph_delta: nx.Graph, style: DotStyle = None):
         self.graph_delta = graph_delta
-        self.old_color = old_color
-        self.new_color = new_color
-        self.dot = Dot()
+        self.style = style or DotStyle()
         self.nodes = {}
 
-    def find_parent(self, parents_with_state, parent=None):
+    def _find_parent(self, dot: Dot, parents_with_state, parent=None):
         key = tuple(map(lambda p: p[0], parents_with_state))
         parent_node = self.nodes.get(key, None)
         if not parent_node:
             if len(parents_with_state) == 1:
                 parent_name, state = parents_with_state[0]
-                parent_node = self.dot.new_item(parent_name, parent=parent)
+                parent_node = dot.new_item(parent_name, parent=parent)
                 color = None
                 if state == "newer":
-                    color = self.new_color
+                    color = self.style.new_color
                 elif state == "older":
-                    color = self.old_color
+                    color = self.style.old_color
                 if color:
-                    self.dot.property_append(parent_node, "color", color)
-                    self.dot.property_append(parent_node, "fontcolor", color)
+                    dot.property_append(parent_node, "color", color)
+                    dot.property_append(parent_node, "fontcolor", color)
                 self.nodes[key] = parent_node
             else:
-                grand_parent = self.find_parent(parents_with_state[0:-1])
-                parent_node = self.find_parent(parents_with_state[-1:], parent=grand_parent)
+                grand_parent = self._find_parent(dot, parents_with_state[0:-1])
+                parent_node = self._find_parent(dot, parents_with_state[-1:], parent=grand_parent)
                 self.nodes[key] = parent_node
         return parent_node
 
     def gen_delta(self, file=Path("output/graph.dot")):
-        self.dot.node_style_default_append("shape", "rectangle")
-        self.dot.node_style_default_append("fontname", "Courier New")
-        self.dot.subgraph_style_default_append("shape", "rectangle")
-        self.dot.subgraph_style_default_append("fontname", "Courier New")
+        dot = self.dot
+        os.makedirs(Path(file).parent, exist_ok=True)
+        with open(file, "w") as file:
+            dot.dot(file)
+
+    @cached_property
+    def dot(self) -> Dot:
+        dot = Dot()
+        dot.node_style_default_append("shape", "rectangle")
+        dot.node_style_default_append("fontname", "Courier New")
+        dot.subgraph_style_default_append("shape", "rectangle")
+        dot.subgraph_style_default_append("fontname", "Courier New")
         new_nodes = self.graph_delta.nodes(data="new", default=False)
         old_nodes = self.graph_delta.nodes(data="old", default=False)
         parents = self.graph_delta.nodes(data="parent", default=None)
@@ -50,29 +64,27 @@ class Renderer(object):
             parent_node = None
             node_parent = parents[node]
             if node_parent:
-                parent_node = self.find_parent(node_parent)
+                parent_node = self._find_parent(dot, node_parent)
             label = labels[node] or node
-            dot_node = self.dot.new_item(label, parent=parent_node)
+            dot_node = dot.new_item(label, parent=parent_node)
             self.nodes[node] = dot_node
             color = None
             if new_nodes[node]:
-                color = self.new_color
+                color = self.style.new_color
             elif old_nodes[node]:
-                color = self.old_color
+                color = self.style.old_color
             if color:
-                self.dot.property_append(dot_node, "color", color)
-                self.dot.property_append(dot_node, "fontcolor", color)
+                dot.property_append(dot_node, "color", color)
+                dot.property_append(dot_node, "fontcolor", color)
         for u, v, data in self.graph_delta.edges.data():
-            link = self.dot.new_link(self.nodes[u], self.nodes[v])
+            link = dot.new_link(self.nodes[u], self.nodes[v])
             color = None
             if data.get("new"):
-                color = self.new_color
+                color = self.style.new_color
             elif data.get("old"):
-                color = self.old_color
+                color = self.style.old_color
             if color:
-                self.dot.property_append(link, "color", color)
+                dot.property_append(link, "color", color)
             if data.get("indirect"):
-                self.dot.property_append(link, "style", "dashed")
-        os.makedirs(Path(file).parent, exist_ok=True)
-        with open(file, "w") as file:
-            self.dot.dot(file)
+                dot.property_append(link, "style", "dashed")
+        return dot
